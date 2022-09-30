b829eac2fbd657bd247ede95473606198460e8b3
Ednaldo Cyriaco <97308257+Edcyriaco@users.noreply.github.com>
2022-09-29 22:46:03 -0300

    SA2 - Encontro Remoto 5
    Encontro Remoto 5- UC12 - SA2

--- Program.cs
+++ Program.cs
@@ -1,90 +1,74 @@
-﻿namespace FS6_Sistema_UC12{
-    class Program{
+﻿namespace FS6_Sistema_UC12
+{
+    class Program
+    {
         static void Main(string[] args)
         {
-          
-       /* Endereco end = new Endereco();
-        end.logradouro = "Rua X";
-        end.numero = 100;
-        end.complemento = "Perto do senai";
-        end.enderecoComercial = false;
-
-        PessoaFisica Maria = new PessoaFisica();
-        Maria.endereco =end;
-        Maria.cpf = "12345";
-        Maria.dataNasc = new DateTime (2022, 08, 02, 02,35,23);
-        Maria.Nome = "Maria Pereira";
-
-        
-       // Console.WriteLine(Maria.endereco.logradouro);
-        //Console.WriteLine(Maria.endereco.numero);
-        //Console.WriteLine(Maria.endereco.complemento);
-        //Console.WriteLine(Maria.endereco.enderecoComercial);
-        //Console.WriteLine(Maria.Nome);
-        //Console.WriteLine(Maria.cpf);
-        //Console.WriteLine(Maria.dataNasc);
-
-        // O ______ , cujo cpf é ____ e faz aniversario no dia ___ mora na ____ numero ____
-           //concatenação
-        //Console.WriteLine("A " + Maria.Nome + ", cujo CPF é " + Maria.cpf + " e nasceu no dia " + Maria.dataNasc + " mora na " + Maria.endereco.logradouro + " numero " + Maria.endereco.numero );
-
-    //interpolação
-        //Console.WriteLine($"A { Maria.Nome}, cujo CPF é {Maria.cpf}, e faz aniversario no dia {Maria.dataNasc} mora na {Maria.endereco.logradouro} numero {Maria.endereco.numero} ");
-               //Console.WriteLine(Maria.ValidarDataNascimento(Maria.dataNasc));
-              bool idadeValidada =(Maria.ValidarDataNascimento(Maria.dataNasc));
-
-              if(idadeValidada == true)
-              {
-                Console.WriteLine(" Seu Cadastro Foi Aprovado Com Sucesso ");
-                  Console.BackgroundColor = ConsoleColor.Red;
-                Console.ForegroundColor = ConsoleColor.DarkGreen;
-              Console.WriteLine("*************************************************************************************************************************");
-              Console.WriteLine("*                                 SENAI  -  Cadastro de Pessoas                                                         *");
-              Console.WriteLine("*                                                                                                                       *");
-              Console.WriteLine("*                                                                                                                       *");
-              Console.WriteLine("*                                                                                                                       *");
-              Console.WriteLine("*                             Seu Cadastro Foi Aprovado Com Sucesso                                                     *");                              
-              Console.WriteLine("*                                                                                                                       *");
-              Console.WriteLine("*************************************************************************************************************************");
-              Console.ResetColor();
-
-              }else{
-                Console.BackgroundColor = ConsoleColor.Black;
-                Console.ForegroundColor = ConsoleColor.DarkGreen;
-              Console.WriteLine("*************************************************************************************************************************");
-              Console.WriteLine("*                                 SENAI  -  Cadastro de Pessoas                                                         *");
-              Console.WriteLine("*                                                                                                                       *");
-              Console.WriteLine("*                                                                                                                       *");
-              Console.WriteLine("*                                                                                                                       *");
-              Console.WriteLine("*                     Seu Cadastro Foi Reprovado Por Motivos de Idade Minima Não Atingida                               *");                              
-              Console.WriteLine("*                                                                                                                       *");
-              Console.WriteLine("*************************************************************************************************************************");
-              Console.ResetColor();
-              }
-*/
-       PessoaJuridica Novapj = new PessoaJuridica ();
-       Endereco end = new Endereco();
-        end.logradouro = "Rua X";
-        end.numero = 100;
-        end.complemento = "Perto do senai";
-        end.enderecoComercial = false;   
-
-        Novapj.endereco = end;
-        Novapj.cnpj = "12345678990001";
-        Novapj.razaoSocial = "Pessoa Juridica";
-
-        bool ValidarCNPJ =Novapj.ValidarCNPJ(Novapj.cnpj);
-        
-        if(Novapj.ValidarCNPJ(Novapj.cnpj)){
-          Console.WriteLine( "CNPJ Valido" );
-        }else{
-          Console.WriteLine( "CNPJ Invalido" );
-                  }               
-
-    
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Write("Iniciando ");
+            Thread.Sleep(500);
 
 
+            for (var contador = 0; contador < 10; contador++)
+            {
+                Console.Write(".");
+                Thread.Sleep(500);
+            }
+            Console.Clear();
+
+            string opcao;
+            do
+            {
+                Console.WriteLine(@$"
+=================================================
+*                                               *
+*                                               *
+*        Seja Bem Vindo ao nosso Sistema        *
+*          de Cadastro de Pessoa                *
+*            Fisica e Juridica                  *
+*                                               *
+*                                               *
+=================================================
+
+
+=================================================
+|          Escolha uma das opcoes abaixo        |           
+=================================================
+!             1 - Pessoa Fisica                 !
+!             2 - Pessoa Juridica               !
+!                                               !
+!               0 - Sair                        !
+!                                               !
+=================================================
+");
+
+
+                opcao = Console.ReadLine();
+
+                switch (opcao)
+                {
+                    case "1":
+                        break;
+                    case "2":
+                        break;
+                    case "0":
+                        break;
+                    default:
+                        Console.WriteLine("Opcao invalida, por favor digite uma das opcaoes apresentadas ");
+                        break;
+
+
+                }
+
+
+            } while (opcao != "0");
+            Console.ResetColor();
+
+
+            {
+
+            }
         }
     }
-
 }
